9f96f70121b6ee72ee8083e9ba6e93b6c1998c50
Carl Pearson <pearson@illinois.edu>
2017-02-15 10:18:15 -0600

    Add nvidia-378 library search path

--- cgoflags_linux.go
+++ cgoflags_linux.go
@@ -6,5 +6,5 @@ package cudainfo
 
 //#cgo LDFLAGS: -lcudart_static -lnvidia-ml -ldl -Wl,--unresolved-symbols=ignore-in-object-files
 //#cgo CFLAGS: -I/usr/local/cuda/include -I /usr/include/nvidia/gdk
-//#cgo LDFLAGS: -L/usr/local/cuda/lib64 -L /usr/src/gdk/nvml/lib/ -L /usr/lib/nvidia-367 -L /usr/lib/nvidia-377
+//#cgo LDFLAGS: -L/usr/local/cuda/lib64 -L /usr/src/gdk/nvml/lib/ -L /usr/lib/nvidia-367 -L /usr/lib/nvidia-377 -L /usr/lib/nvidia-378
 import "C"
